13a558d6ac1476431fd312683c7913772e7fb00e
Nikita Dudnik aka Nek <nikdudnik@gmail.com>
2013-08-02 01:27:37 +0700

    Randomize tiles

--- Main.elm
+++ Main.elm
@@ -1,3 +1,5 @@
+import Random
+
 data Tile = Grass | Sand | Rock | Water
 
 colorOf tile =
@@ -7,24 +9,38 @@ colorOf tile =
         Rock -> rgba 125 125 125 1
         Water -> rgba 125 125 255 1
 
-level = [
-    Grass, Grass, Grass, Grass, Grass, Grass, Grass, Grass,    
-    Grass, Grass, Grass, Grass, Grass, Grass, Grass, Grass,
-    Grass, Grass, Grass, Grass, Grass, Grass, Grass, Grass,
-    Grass, Grass, Grass, Rock, Grass, Grass, Grass, Grass,
-    Grass, Grass, Grass, Grass, Grass, Grass, Grass, Grass,
-    Grass, Grass, Grass, Grass, Grass, Grass, Grass, Grass,
-    Grass, Grass, Grass, Grass, Grass, Grass, Grass, Grass,
-    Grass, Grass, Grass, Grass, Grass, Grass, Grass, Grass
-    ]
+tileWidth = 16
+tileHeight = 16
+sceneWidth = 480
+sceneHeight = 320
+levelWidth = 12
+levelHeight = 8
 
-makeShape : Tile -> Int -> Form
-makeShape tile coord =
-    let x = coord `mod` 8 in 
-    let y = coord `div` 8 in
-    filled (colorOf tile) (rect 32 32) |> move (x * 32 - 240 
-                                       |> toFloat, y * (0-32) + 160  
+
+makeTile n = head (drop n [Grass, Rock, Sand, Water])
+
+makeShape : Int -> Int -> Tile -> Form
+makeShape x y tile=
+    let tw = tileWidth in
+    let th = tileHeight in
+    let w = sceneWidth `div` 2 in
+    let h = sceneHeight `div` 2 in  
+    filled (colorOf <| tile) (rect tw th) |> move (x * tw - 0 
+                                       |> toFloat, y * (0-th) + 0 
                                        |> toFloat)
 
-main = collage 480 320 
-    (zipWith makeShape level [0..63])
+makeBlock x y = 
+    ((makeShape x y) <~ (makeTile <~ Random.range 0 2 (constant Nothing)))
+
+makeNthBlock n = 
+    let x = n `mod` levelWidth in
+    let y = n `div` levelWidth in
+    makeBlock x y
+
+makeCollage : [Form] -> Element
+makeCollage fs =
+    collage sceneWidth sceneHeight fs
+
+makeBlocksList = combine (map makeNthBlock [0..(levelWidth * levelHeight - 1)])
+
+main = makeCollage <~ makeBlocksList
